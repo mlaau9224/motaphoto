8d169b416ef9a7d7e495d55fb35ddd8f42d59c49
Michael Laau <mlaau9224@gmail.com>
2025-10-17 19:49:42 +0200

    Ajout de la liste des photos

--- index.php
+++ index.php
@@ -14,7 +14,17 @@ $args = array(
     ],
 );
 
+$paged = get_query_var('paged') ? get_query_var('paged') : 1;
+
 $hero = new WP_Query($args);
+
+$args2 = array(
+    'post_type' => 'photo',
+    'posts_per_page' => 8,
+    'paged' => $paged,
+);
+
+$photos = new WP_Query($args2);
 ?>
 
 <?php
@@ -30,7 +40,15 @@ get_header();
             <h1>Photographe Event</h1>
         </div>  
     </div>
-<?php endif; ?>    
+<?php endif; ?>
+
+<?php if($photos->have_posts()): ?>
+    <div class="liste-photos">
+        <?php while($photos->have_posts()) : $photos->the_post(); ?>
+            <?= get_template_part('templates_part/photo_block'); ?>
+        <?php endwhile; ?>
+    </div>
+<?php endif; ?>
 
 <?php
 get_footer();
